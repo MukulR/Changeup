db6433a6768762b5dc814cf63a2c47a74c3ebecb
MukulR <mukulgrao@gmail.com>
2020-10-28 20:07:00 -0700

    Delete unnecessary files

--- src/commons.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef _USER_CONTROL_H_
-#define _USER_CONTROL_H_
-
-void driveRobot(double degrees, int speed, void* param);
-void driveRobotBack(double degrees, int speed, void* param);
-void turnWithoutSensors(double degrees, void* param);
-
-#endif //_USER_CONTROL_H_
-
