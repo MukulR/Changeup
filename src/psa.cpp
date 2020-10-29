030ca25c0e78925b74e9ac0def522a5b530cc9ef
MukulR <mukulgrao@gmail.com>
2020-10-28 20:03:19 -0700

    Refactored code to previous structure.

---
+++ src/psa.cpp
@@ -0,0 +1,16 @@
+#include "motordefs.hpp"
+#include "psa.hpp"
+
+ProgrammingSkillsAuton::ProgrammingSkillsAuton(MotorDefs *md, bool ra){
+    mtrDefs = md;
+    redAlliance = ra;
+}
+
+ProgrammingSkillsAuton::~ProgrammingSkillsAuton() {
+
+}
+
+void ProgrammingSkillsAuton::runAuton(){
+    
+}
+
